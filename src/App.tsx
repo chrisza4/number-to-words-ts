08e881c5a4ec35ca7fd7e965ecc1e307b0fbc7c4
chrisza4 <chakrit.lj@gmail.com>
2020-12-25 12:25:29 +0700

    Add UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
-import React from 'react'
-import logo from './logo.svg'
+import React, { useState, useRef } from 'react'
+import { numberToWords } from './numberToWords/numberToWords'
 import './App.css'
 
+type OnButtonClick = (
+  event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+) => void
+
+function capitalize(str: string): string {
+  return str[0].toUpperCase() + str.slice(1)
+}
+
 function App(): JSX.Element {
+  const [answer, setAnswer] = useState('')
+  const textRef = useRef<HTMLInputElement>(null)
+  const onTranslateClick: OnButtonClick = () => {
+    if (!textRef.current?.value) {
+      return
+    }
+    const number = parseInt(textRef.current.value)
+    if (isNaN(number)) {
+      return setAnswer('Please enter a valid number')
+    }
+    setAnswer(capitalize(numberToWords(number)))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Enter a number</p>
+      <p>
+        <input ref={textRef} type="text" />
+        <button onClick={onTranslateClick}>Translate</button>
+      </p>
+      <p>{answer}</p>
     </div>
   )
 }
